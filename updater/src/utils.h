8c638ead0d21c9fc7b9bf26b7af74943e3582141
Restyled.io <commits@restyled.io>
2023-11-18 17:35:45 +0000

    Restyled by clang-format

--- updater/src/utils.h
+++ updater/src/utils.h
@@ -1,97 +1,98 @@
 #pragma once
 
-#include <vector>
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <vector>
 
 #include "cppcrc.h"
 
 typedef struct Version {
-    uint32_t major;
-    uint32_t minor;
-    uint32_t patch;
+  uint32_t major;
+  uint32_t minor;
+  uint32_t patch;
 } Version;
 
 void LaunchGame();
 void UninstallEAC();
 
-inline uint16_t ComputeCRC16Hash(std::ifstream& file) {
-    std::vector<uint8_t> buffer(std::istreambuf_iterator<char>(file), {});
-    return CRC16::CCITT_FALSE::calc(buffer.data(), buffer.size());
+inline uint16_t ComputeCRC16Hash(std::ifstream &file) {
+  std::vector<uint8_t> buffer(std::istreambuf_iterator<char>(file), {});
+  return CRC16::CCITT_FALSE::calc(buffer.data(), buffer.size());
 }
 
 inline std::string VersionString(Version version) {
-    return std::to_string(version.major) + "." + std::to_string(version.minor) + "." + std::to_string(version.patch);
+  return std::to_string(version.major) + "." + std::to_string(version.minor) +
+         "." + std::to_string(version.patch);
 }
 
 inline bool IsBiggerVersion(Version a, Version b) {
-    if(a.major > b.major) {
-        return true;
-    }
+  if (a.major > b.major) {
+    return true;
+  }
 
-    if(a.major < b.major) {
-        return false;
-    }
-
-    if(a.minor > b.minor) {
-        return true;
-    }
-
-    if(a.minor < b.minor) {
-        return false;
-    }
-
-    if(a.patch > b.patch) {
-        return true;
-    }
-
-    if(a.patch < b.patch) {
-        return false;
-    }
-
+  if (a.major < b.major) {
     return false;
+  }
+
+  if (a.minor > b.minor) {
+    return true;
+  }
+
+  if (a.minor < b.minor) {
+    return false;
+  }
+
+  if (a.patch > b.patch) {
+    return true;
+  }
+
+  if (a.patch < b.patch) {
+    return false;
+  }
+
+  return false;
 }
 
 inline Version ParseVersion(std::string version) {
-    Version v;
-    std::string::iterator it = version.begin();
-    std::string::iterator end = version.end();
+  Version v;
+  std::string::iterator it = version.begin();
+  std::string::iterator end = version.end();
 
-    std::string major;
-    std::string minor;
-    std::string patch;
+  std::string major;
+  std::string minor;
+  std::string patch;
 
-    for(; it != end; it++) {
-        if(*it == '.') {
-            break;
-        }
-
-        major += *it;
+  for (; it != end; it++) {
+    if (*it == '.') {
+      break;
     }
 
-    it++;
+    major += *it;
+  }
 
-    for(; it != end; it++) {
-        if(*it == '.') {
-            break;
-        }
+  it++;
 
-        minor += *it;
+  for (; it != end; it++) {
+    if (*it == '.') {
+      break;
     }
 
-    it++;
+    minor += *it;
+  }
 
-    for(; it != end; it++) {
-        if(*it == '.') {
-            break;
-        }
+  it++;
 
-        patch += *it;
+  for (; it != end; it++) {
+    if (*it == '.') {
+      break;
     }
 
-    v.major = std::stoi(major);
-    v.minor = std::stoi(minor);
-    v.patch = std::stoi(patch);
+    patch += *it;
+  }
 
-    return v;
+  v.major = std::stoi(major);
+  v.minor = std::stoi(minor);
+  v.patch = std::stoi(patch);
+
+  return v;
 }
