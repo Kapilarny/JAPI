1a6aa63102770aa5abefd7ec214fa7e583a95481
Kapilarny <kapilarnyyt@gmail.com>
2023-12-06 18:11:33 +0100

    change offset for CPK hooks in connections

--- src/utils/game_data.cpp
+++ src/utils/game_data.cpp
@@ -12,7 +12,7 @@ GameData::GameData() {
     } else if(std::filesystem::exists("NSUNSC.exe")) {
         this->game_type = GameType::CONNECTIONS;
 
-        this->functions.CPKPreloadFunc = 0xa7eed0;
-        this->functions.CPKLoadFunc = 0xa7ef58;
+        this->functions.CPKPreloadFunc = 0xA7EED0;
+        this->functions.CPKLoadFunc = 0x5F7850;
     }
 }
