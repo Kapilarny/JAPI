27f53cdaa1c26786be6e72dc7fe7c5528fc18a0c
Kapilarny <kapilarnyyt@gmail.com>
2024-09-02 15:26:03 +0200

    me is a dumbo

--- src/utils/cpk.cpp
+++ src/utils/cpk.cpp
@@ -120,5 +120,5 @@ void LoadCPKMods() {
         "sub_645828"
     };
 
-    JAPI_HookFunction(&hook);
+    JAPI_HookASBRFunction(&hook);
 }
--- src/utils/game_data.cpp
+++ src/utils/game_data.cpp
@@ -12,21 +12,21 @@ GameData::GameData() {
         this->game_type = GameType::ASBR;
 
         // Try scanning for functions
-        this->functions.CPKPreloadFunc = GAME_SCAN("48 89 5C 24 ? 48 89 7C 24 ? 41 56 48 83 EC ? 48 83 3D");
-        this->functions.CPKLoadFunc = GAME_SCAN("48 83 EC ? 44 8B C2 48 8D 54 24 ? E8 ? ? ? ? 48 83 C4");
+        // this->functions.CPKPreloadFunc = GAME_SCAN("48 89 5C 24 ? 48 89 7C 24 ? 41 56 48 83 EC ? 48 83 3D");
+        // this->functions.CPKLoadFunc = GAME_SCAN("48 83 EC ? 44 8B C2 48 8D 54 24 ? E8 ? ? ? ? 48 83 C4");
 
-        if(!this->functions.CPKLoadFunc || !this->functions.CPKPreloadFunc) {
-            JWARN("Couldn't find the game functions! Defaulting to hardcoded!");
+        // if(!this->functions.CPKLoadFunc || !this->functions.CPKPreloadFunc) {
+        //     JWARN("Couldn't find the game functions! Defaulting to hardcoded!");
+        // }
 
-            this->functions.CPKPreloadFunc = JAPI_GetASBRModuleBase() + 0x56C970;
-            this->functions.CPKLoadFunc = JAPI_GetASBRModuleBase() + 0x662378;
-        }
+        this->functions.CPKPreloadFunc = 0x56C970;
+        this->functions.CPKLoadFunc = 0x662378;
     } else if(std::filesystem::exists("NSUNSC.exe")) {
         this->game_type = GameType::CONNECTIONS;
 
         // TODO: Pattern scan for NSUNSC too
 
-        this->functions.CPKPreloadFunc = JAPI_GetASBRModuleBase() + 0xA7EED0;
-        this->functions.CPKLoadFunc = JAPI_GetASBRModuleBase() + 0x5F7850;
+        this->functions.CPKPreloadFunc = 0xA7EED0;
+        this->functions.CPKLoadFunc = 0x5F7850;
     }
 }
